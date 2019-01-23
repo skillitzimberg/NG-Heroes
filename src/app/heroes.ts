90e50ab0685864c8dbf40b7af948593958de96b3
Ralph Perdomo and Scott Bergler <scott@>
2019-01-23 11:42:18 -0800

    add hero list and detail display.

---
+++ src/app/heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  new Hero(1, 'Windstorm'),
+  new Hero(2, 'me'),
+  new Hero(3, 'Mooch'),
+  new Hero(4, 'Ted'),
+  new Hero(5, 'Guillermo'),
+  new Hero(6, 'Godot'),
+  new Hero(7, 'Cinco')
+];
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  
+  constructor() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  ngOnInit() {
+  }
+
+}
